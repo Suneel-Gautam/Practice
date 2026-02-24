f235d16f839fb9d9e7f855d8f9e189da08d765c5
Sunil Gautam <gautamsunil917@gmail.com>
2026-02-24 11:30:29 +0545

    formValidation with visual interface

---
+++ frontendtasks/formValidation/script.js
@@ -0,0 +1,25 @@
+console.log("we are running");
+
+const submitButton = document.getElementById("submitBtn");
+const username = document.getElementById("username");
+const message = document.getElementById("message");
+
+submitButton.addEventListener("click", () => {
+  const isValid = username.value.length >= 5;
+
+  if (isValid) {
+    message.classList.add("success");
+    message.classList.remove("neutral");
+    message.classList.remove("error");
+    username.classList.add("usernameSuccess");
+    username.classList.remove("usernameError");
+    message.innerHTML = "Username is Valid";
+  } else {
+    message.classList.add("error");
+    message.classList.remove("neutral");
+    message.classList.remove("success");
+    username.classList.add("usernameError");
+    username.classList.remove("usernameSuccess");
+    message.innerHTML = "Username is Less than 5";
+  }
+});
