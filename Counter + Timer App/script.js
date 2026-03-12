a843d87479d48a7746bd42ff16a18220cfdb07b0
Sunil Gautam <gautamsunil917@gmail.com>
2026-03-12 11:30:03 +0545

    counter and timer completed

--- Counter + Timer App/script.js
+++ Counter + Timer App/script.js
@@ -26,15 +26,49 @@ const pauseBtn = document.getElementById("pause-btn");
 const stopBtn = document.getElementById("stop-btn");
 const stopWatch = document.getElementById("Stop-Watch");
 
-let hour = 0
-let minutes = 0 
-let seconds = 0 
-let miliseconds = 0 
+let hour = 0;
+let minutes = 0;
+let seconds = 0;
+let miliseconds = 0;
+let interval = null;
 
 function startStopwatch() {
-    startBtn.addEventListener('click' , () =>{
-        
-    })
+  miliseconds += 10;
 
+  if (miliseconds >= 1000) {
+    miliseconds = 0;
+    seconds++;
+  }
 
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  if (minutes >= 60) {
+    minutes = 0;
+    hour++;
+  }
+
+  stopWatch.innerHTML = `${hour.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}:${miliseconds}`;
 }
+
+startBtn.addEventListener("click", () => {
+  if (interval) return;
+  interval = setInterval(startStopwatch, 10);
+});
+
+pauseBtn.addEventListener("click", () => {
+  clearInterval(interval);
+  interval = null;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(interval);
+  interval = null;
+  hour = 0;
+  minutes = 0;
+  seconds = 0;
+  miliseconds = 0;
+  stopWatch.innerHTML = "00:00:00.000";
+});
