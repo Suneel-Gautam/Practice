1e8f18b010f8029fe0492577ba2105dfbfc47a0d
Sunil Gautam <gautamsunil917@gmail.com>
2026-03-16 16:43:47 +0545

    advance quiz javascript started

--- Quizz-Advanced/script.js
+++ Quizz-Advanced/script.js
@@ -1,1 +1,5 @@
-console.log('We are running')
+console.log("We are running");
+
+const startSection = document.getElementById("Start-quizz");
+const questionSection = document.getElementById("Question-Section");
+const resultSection = document.getElementById("Result-Section");
