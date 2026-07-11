4371dd0f4f0ee1984ce3982b7fabba80a16b6514
Sunil Gautam <gautamsunil917@gmail.com>
2026-07-11 15:27:59 +0545

    sloved some basics questions

---
+++ Problem solving/printNumber.js
@@ -0,0 +1,8 @@
+function printNumber(num) {
+
+    for (let i = 1; i <= num; i++) {
+        console.log(i)
+    }
+
+}
+printNumber(0)
---
+++ Problem solving/printReverse.js
@@ -0,0 +1,8 @@
+function printReverse(n) {
+
+    for (let i = n; i > 0; i--) {
+        console.log(i)
+    }
+}
+
+printReverse(5)
